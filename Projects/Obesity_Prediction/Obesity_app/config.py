2cc6ea09f732b39a5c32981467d6987fc90c3be6
IamGuts <femto_wing_of_darkness@mail.ru>
2025-02-05 16:37:35 +0300

    Obesity project analitics and web

---
+++ Projects/Obesity_Prediction/Obesity_app/config.py
@@ -0,0 +1,2 @@
+MODEL_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Projects\\Obesity_Prediction\\model\\model.pkl'
+IMAGE_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Projects\\Obesity_Prediction\\Obesity_app\\ozhirenie.jpg'
