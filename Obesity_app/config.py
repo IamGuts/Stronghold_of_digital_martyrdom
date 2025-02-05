72f5440e2c5fef3e384b723aeb23767d1998b2c7
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-05 20:03:29 +0300

    12

--- Obesity_app/config.py
+++
@@ -1,2 +0,0 @@
-MODEL_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Data-Science\\Data-Science\\Obesity_app\\model\\model.pkl'
-IMAGE_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Data-Science\\Data-Science\\Obesity_app\\imt-calculator.jpg'
