056c72347620ad2d47cea3d49d4d2c0e266b898f
Ярослав Савик <126880089+IamGuts@users.noreply.github.com>
2025-03-10 14:37:54 +0300

    10.03.2025

---
+++ config.py
@@ -0,0 +1,6 @@
+my_sql_connect = {
+    'user' : 'uzmr8osmluawgb9z',
+    'host' : 'beq2knepwd7hdzgxmrqj-mysql.services.clever-cloud.com',
+    'Database_name' : 'beq2knepwd7hdzgxmrqj',
+    'password' : 'j0qYZrz34tTCzJUfcJxS',
+}
