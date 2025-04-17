47c486fe70e4f89e48709b31e32406ad64ef1fff
Ярослав Савик <126880089+IamGuts@users.noreply.github.com>
2025-04-17 20:13:53 +0300

    final

--- Projects/Obesity_Prediction/Obesity_app/config.py
+++
@@ -1,2 +0,0 @@
-MODEL_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Projects\\Obesity_Prediction\\model\\model.pkl'
-IMAGE_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\Projects\\Obesity_Prediction\\Obesity_app\\ozhirenie.jpg'
