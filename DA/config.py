f79ea3e5827c0f356acfa5129f90390a972d0a7d
Ярослав Савик <126880089+IamGuts@users.noreply.github.com>
2025-05-08 21:28:55 +0300

    проба

---
+++ DA/config.py
@@ -0,0 +1,8 @@
+mysql_server = {
+    'my_host' : 'beq2knepwd7hdzgxmrqj-mysql.services.clever-cloud.com',
+    'my_db_name' : 'beq2knepwd7hdzgxmrqj',
+    'my_user' : 'uzmr8osmluawgb9z',
+    'my_password' : 'j0qYZrz34tTCzJUfcJxS',
+    'my_port' : '3306',
+    'my_connection_url' : 'mysql://uzmr8osmluawgb9z:j0qYZrz34tTCzJUfcJxS@beq2knepwd7hdzgxmrqj-mysql.services.clever-cloud.com:3306/beq2knepwd7hdzgxmrqj'
+    }
